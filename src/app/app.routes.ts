9dcfbdb3698f2e2ceb887f80ef4ebe60b2f836c7
Cekas <cekas@hotmail.com>
2018-11-12 11:12:48 +0100

    Modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,14 @@
 import { Routes, RouterModule } from "@angular/router";
 
-import { PagesComponent } from "./pages/pages.component";
-
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-    import { ProgressComponent } from "./pages/progress/progress.component";
-    import { Graficas1Component } from "./pages/graficas1/graficas1.component";
-    import { NoPageFoundComponent } from "./shared/no-page-found/no-page-found.component";
-
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./login/register.component";
+
+import { NoPageFoundComponent } from "./shared/no-page-found/no-page-found.component";
 
 // Archivo de rutas pricipales
 
 const appRoutes: Routes = [
-    {
-        // children Rutas hijas que van a trabajar dentro del componente PagesComponent
-        path:'', // La Ruta raiz vacia lleva al componente PagesComponent, el cual tiene hijas
-        component: PagesComponent,
-        children: [
-            { path:'dashboard', component: DashboardComponent },
-            { path:'progress', component: ProgressComponent },
-            { path:'gravicas1', component: Graficas1Component },
-            { path:'', redirectTo: '/dashboard', pathMatch: 'full'}, // Ruta, Cuando no existe ninguna ruta.
 
-        ]
-    },
     { path:'login', component: LoginComponent },
     { path:'register', component: RegisterComponent },
     // {path:'', redirectTo: '/dashboard', pathMatch: 'full'}, // Ruta, Cuando no existe ninguna ruta.
